16fd88b3eea8673a842b847afd39d61080272dcb
Guillaume Pelletier <guigui.p@gmail.com>
2022-07-06 19:06:18 -0700

    Add some boolean operators

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -5,8 +5,18 @@ import           Data.Functor
 import           LispError
 import           LispVal
 
-unpackStringNum :: String -> ThrowsError Integer
-unpackStringNum n =
+unpackBool :: LispVal -> ThrowsError Bool
+unpackBool (Bool b) = return b
+unpackBool notBool  = throwError $ TypeMismatch "boolean" notBool
+
+unpackStr :: LispVal -> ThrowsError String
+unpackStr (String s) = return s
+unpackStr (Number s) = return $ show s
+unpackStr (Bool s)   = return $ show s
+unpackStr notString  = throwError $ TypeMismatch "string" notString
+
+unpackStrNum :: String -> ThrowsError Integer
+unpackStrNum n =
     let parsed = reads n in
         if null parsed
             then throwError $ TypeMismatch "number" $ String n
@@ -14,7 +24,7 @@ unpackStringNum n =
 
 unpackNum :: LispVal -> ThrowsError Integer
 unpackNum (Number n) = return n
-unpackNum (String n) = unpackStringNum n
+unpackNum (String n) = unpackStrNum n
 unpackNum (List [n]) = unpackNum n
 unpackNum notNum     = throwError $ TypeMismatch "number" notNum
 
@@ -23,14 +33,39 @@ numericBinop op []            = throwError $ NumArgs 2 []
 numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
 numericBinop op params        = mapM unpackNum params <&> (Number . foldl1 op)
 
+boolBinop :: (LispVal -> ThrowsError a) -> (a -> a -> Bool) -> [LispVal] -> ThrowsError LispVal
+boolBinop unpacker op args =
+    if length args /= 2
+        then throwError $ NumArgs 2 args
+        else do left  <- unpacker $ head args
+                right <- unpacker $ args !! 1
+                return $ Bool $ left `op` right
+
+numBoolBinop = boolBinop unpackNum
+strBoolBinop = boolBinop unpackStr
+boolBoolBinop = boolBinop unpackBool
+
 primitives :: [(String, [LispVal] -> ThrowsError LispVal)]
-primitives = [("+", numericBinop (+)),
-              ("-", numericBinop (-)),
-              ("*", numericBinop (*)),
-              ("/", numericBinop div),
-              ("mod", numericBinop mod),
-              ("quotient", numericBinop quot),
-              ("remainder", numericBinop rem)]
+primitives = [("+",         numericBinop (+)),
+              ("-",         numericBinop (-)),
+              ("*",         numericBinop (*)),
+              ("/",         numericBinop div),
+              ("mod",       numericBinop mod),
+              ("quotient",  numericBinop quot),
+              ("remainder", numericBinop rem),
+              ("=",         numBoolBinop (==)),
+              ("<",         numBoolBinop (<)),
+              (">",         numBoolBinop (>)),
+              ("/=",        numBoolBinop (/=)),
+              (">=",        numBoolBinop (>=)),
+              ("<=",        numBoolBinop (<=)),
+              ("&&",        boolBoolBinop (&&)),
+              ("||",        boolBoolBinop (||)),
+              ("string=?",  strBoolBinop (==)),
+              ("string<?",  strBoolBinop (<)),
+              ("string>?",  strBoolBinop (>)),
+              ("string<=?", strBoolBinop (<=)),
+              ("string>=?", strBoolBinop (>=))]
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply f args =
